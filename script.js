a7bea768f0e3bb5be3b75919b4b447f61f896ed6
jacksolaro <jacksolaro@gmail.com>
2020-09-16 14:24:49 -0700

    hid start button on click. Prompt user for initials after game over

--- script.js
+++ script.js
@@ -1,70 +1,135 @@
-var timer = document.querySelector("#timer");
-var startBtn = document.querySelector("#startBtn");
-var questionTextEl = document.querySelector("#questionText");
+// ================================================
+//  DOM ELEMENTS
+// ================================================
+var timerEl = $("#timer");
+var startBtn = $("#startBtn");
+var questionTextEl = $("#questionText");
+var answerBtn = $(".answer-btn");
+var answerEl1 = $("#answer1");
+var answerEl2 = $("#answer2");
+var answerEl3 = $("#answer3");
+var answerEl4 = $("#answer4");
 
-// Number of Seconds on the Timer
+
+// ================================================
+//  Initialize Variables
+// ================================================
+
 var secondsLeft = 100;
+var questionIndex = 0;
 
-function startGame() {
-    runTimer();
-    console.log("Game Started")
-    generateQuestions();
-}
+// TODO: Create Question Content
+var questionList = [
+    {
+        question: "Which singer’s real name is Stefani Joanne Angelina Germanotta?",
+        answer1: "Lady Gaga",
+        answer2: "Madonna",
+        answer3: "Gwen Stefani",
+        answer4: "Lorde",
+        correctAns: "answer1"
+    },
+    {
+        question: "Which of the following is one of the only two egg laying mammals left on the planet today?",
+        answer1: "Common Opossum",
+        answer2: "Aardvark",
+        answer3: "Duck-Billed Platypus",
+        answer4: "Common Bat",
+        correctAns: "answer3"
+    },
+    {
+        question: "What is the tiny piece at the end of a shoelace called?",
+        answer1: "Aglet",
+        answer2: "Lace Fastener",
+        answer3: "Tiplet",
+        answer4: "Lace Tip",
+        correctAns: "answer1"
+    }
+]
 
-// Run Timer
+// ================================================
+//  Page Load
+// ================================================
+timerEl.text(secondsLeft);
+
+
+// ================================================
+//  Functions
+// ================================================
+
+// Start Game
 // TODO: At 10 seconds left, turn text red and bold
-function runTimer() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timer.textContent = secondsLeft + " seconds left";
+startBtn.on("click", function() {
+    // Hide the Start Button
+    startBtn.css({'display':'none'});
 
-        if(secondsLeft <= 0) {
-        clearInterval(timerInterval);
-        console.log("Game Over")
-        }
+    // TODO: Hide the Starter Screen
 
-    }, 1000);
-}
+    // Display Questions
+    generateQuestions();
 
-function generateQuestions() {
-    console.log(questions[0].question);
-    questionTextEl.textContent = questions[0].question;
-}
-
-
-startBtn.addEventListener("click", function(){
-    if(secondsLeft === 100 || secondsLeft === 0) {
-        startGame();
+    // Runs Timer, only if there isn't already a timer running
+    if(secondsLeft === 100 || secondsLeft <= 0){
+        var gameTimer = setInterval(() => {
+            secondsLeft--;
+            timerEl.text(`${secondsLeft}`);
+    
+            // TODO: If time left is 0 OR there are no more questions
+            if(secondsLeft <= 0 || questionIndex === questionList.length) {
+                clearInterval(gameTimer);
+                console.log("Game Over")
+                // TODO: Run gameOver()
+                gameOver();
+            }
+    
+        }, 1000)
     }
 })
 
-// TODO: Create Question Content
-const questions = [
-    {
-        question: "Question 1",
-        answers: [
-            { text: 'Answer 1', correct: true},
-            { text: 'Answer 2', correct: false},
-            { text: 'Answer 3', correct: false},
-            { text: 'Answer 4', correct: false}
-        ]
-    },
-    {
-        question: "Question 2",
-        answers: [
-            { text: 'Answer 1', correct: true},
-            { text: 'Answer 2', correct: false},
-            { text: 'Answer 3', correct: false},
-            { text: 'Answer 4', correct: false}
-        ]
-    },
-    {
-        question: "Question 3",
-        answers: [
-            { text: 'Answer 1', correct: true},
-            { text: 'Answer 2', correct: false},
-            { text: 'Answer 3', correct: false},
-            { text: 'Answer 4', correct: false}
-        ]
-    },
-]
+// Answer Click
+answerBtn.on("click", function(e) {
+    var answerChoice = (e.target);
+    
+    // Check to see if the user selected the correct answer
+    if(answerChoice.id === questionList[questionIndex].correctAns) {
+        // Correct Answer
+        console.log("it works so far")
+    } else {
+        // Incorrect Answer
+        console.log("it works and you clicked a wrong answer")
+        secondsLeft = secondsLeft - 15;
+    }
+    
+    // increase the question index
+    questionIndex++;
+
+    // if there are more questions in the array, generate next question
+    if(questionIndex < questionList.length) {
+        generateQuestions();
+    }
+})
+
+// Generate Questions
+function generateQuestions() {
+    questionTextEl.text(questionList[questionIndex].question);
+    answerEl1.text(questionList[questionIndex].answer1);
+    answerEl2.text(questionList[questionIndex].answer2);
+    answerEl3.text(questionList[questionIndex].answer3);
+    answerEl4.text(questionList[questionIndex].answer4);
+}
+
+// Game Over Function
+function gameOver() {
+    // TODO: Hide all question and answer elements
+    
+    // Prompt user for intials
+    prompt(`Great game! Your score is: ${secondsLeft}! Please enter your intials to save your score: `);
+    
+    // Save score to local storage
+    
+    // Display leaderboard
+
+}
+
+
+
+
